5c927642eb3463234f18058a572b7d712f12f4a0
Duy Ho <baoduy8119@gmail.com>
2022-05-01 14:53:03 +0700

    update code

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,15 +20,30 @@ jQuery.event.special.mousewheel = {
 };
 
 $(document).ready(function () {
-    $('.vision-items .text-item').mouseover(function() {
-        $('.vision-items .active-item').removeClass('active')
-        $(this).next().addClass('active')
-    })
+    $(".vision-items .text-item").mouseover(function () {
+        $(".vision-items .active-item").removeClass("active");
+        $(this).next().addClass("active");
+    });
     $(".toggle-menu").on("click", function (e) {
-        e.preventDefault()
-        $(".nav-mobile").toggleClass("show")
-    })
+        e.preventDefault();
+        $(".nav-mobile").toggleClass("show");
+    });
     $(document).on("click", function (t) {
-        0 !== $("header").has(t.target).length && 1 !== $(".btn-close").has(t.target).length || $(".nav-mobile").removeClass("show")
-    })
+        (0 !== $("header").has(t.target).length && 1 !== $(".btn-close").has(t.target).length) || $(".nav-mobile").removeClass("show");
+    });
+    $("header a").on("click", function (e) {
+        if (this.hash !== "") {
+            e.preventDefault();
+            var hash = this.hash;
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                800,
+                function () {
+                    window.location.hash = hash;
+                }
+            );
+        }
+    });
 });
